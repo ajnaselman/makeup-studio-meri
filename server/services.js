2aca2ef6c8606b23ff1890561266b6c321dbc8fc
ajnaselman <selmanajna@gmail.com>
2025-08-16 20:45:02 +0200

    Update services.js

--- server/services.js
+++ server/services.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { servicesPath, readJSON, writeJSON } = require('./db');
 
-// Dohvati sve usluge
+
 router.get('/', (req, res) => {
     const services = readJSON(servicesPath);
     res.json(services);
 });
 
-// Dodaj novu uslugu
+
 router.post('/', (req, res) => {
     const services = readJSON(servicesPath);
     const newService = { id: Date.now(), ...req.body };
